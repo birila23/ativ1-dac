104bf8f63133264a45c9f65e6b37b1ba7e04f543
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2026-04-12 18:16:08 -0300

    feat: configuring the dependencies of spring boot

---
+++ src/main/java/com/dac/ativ1_spring/Ativ1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.dac.ativ1_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ativ1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ativ1SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dac/ativ1_spring/Ativ1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dac.ativ1_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ativ1SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
